97eb7005d015aa515c897304c616596d2d112379
Day-101 <visualdai@gmail.com>
2021-03-08 16:02:12 +0100

    Added total method

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,10 @@ class Cart < ApplicationRecord
 	belongs_to :user
 	has_many :cart_item_jointables, dependent: :destroy
 	has_many :items, through: :cart_item_jointables
+
+	def total
+		price = 0
+		self.items.each { |item| price += item.price }
+		return price
+	end
 end
