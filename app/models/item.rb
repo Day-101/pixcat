0614915d86c81a431de6d583244eda568574e631
Day-101 <visualdai@gmail.com>
2021-03-10 10:18:18 +0100

    active storage images and seed

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
 	validates :description, length: { maximum: 500}, presence: true
 	validates :price, numericality: { less_than: 100 }, presence: true
 	validates :image_url, format: {with: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/, message: "Enter a valid link"}, presence: true
-	has_many :cart_item_jointable
-	has_many :order_item_jointable
+	has_many :cart_item_jointable, dependent: :destroy
+	has_many :order_item_jointable, dependent: :destroy
+	has_one_attached :cat_picture
 end
