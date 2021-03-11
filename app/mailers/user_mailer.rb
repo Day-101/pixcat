e6bcdd8f79c8ad0d7b7c03fd5e51ca2224cee972
Matteo <kamikorosu@hotmail.fr>
2021-03-11 13:50:39 +0100

    fixed the mail picture for prod

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-    default from: 'visualdai@gmail.com'
+    default from: ENV['GMAIL_LOGIN']
  
     def command_email(order)
       #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
@@ -8,7 +8,7 @@ class UserMailer < ApplicationMailer
       #on définit une variable @url qu'on utilisera dans la view d’e-mail
       @url  = 'http://monsite.fr/login' 
      
-      # attachments['filename.jpg'] = File.read(order.items[0].cat_picture.path)
+       attachments['filename.jpg'] = File.read(order.items[0].cat_picture.tempfile)
       # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
       mail(to: @user.email, subject: "Merci d'avoir passé commande chez nous !") 
       mail(to: "catadmin@yopmail.com", subject: "Un client à passé commande chez nous !") 
