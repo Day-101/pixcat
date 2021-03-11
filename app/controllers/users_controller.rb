e291a225d03874f18e8c3729925e9299224af5be
Day-101 <visualdai@gmail.com>
2021-03-11 16:41:59 +0100

    Tried to fix a wrong mail in prod

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :redirect_if_not_owned, only: [:show]
   def index
     @users = User.all
   end
@@ -12,4 +14,9 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
+
+  def redirect_if_not_owned
+    redirect_to user_path(current_user) unless current_user.id == params[:id].to_i
+  end
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -125,7 +125,7 @@ Rails.application.configure do
   config.assets.digest = true
 
 
-  config.action_mailer.default_url_options = { :host => 'https://pixcat-devellopement.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'https://pixcat-production.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
