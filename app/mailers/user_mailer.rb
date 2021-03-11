135929c0304132be3426489031e420e9a61797d0
Matteo <kamikorosu@hotmail.fr>
2021-03-11 14:06:45 +0100

    fixed the mail picture for prod again

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,7 +8,7 @@ class UserMailer < ApplicationMailer
       #on définit une variable @url qu'on utilisera dans la view d’e-mail
       @url  = 'http://monsite.fr/login' 
      
-       attachments['filename.jpg'] = File.read(order.items[0].cat_picture.tempfile)
+       
       # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
       mail(to: @user.email, subject: "Merci d'avoir passé commande chez nous !") 
       mail(to: "catadmin@yopmail.com", subject: "Un client à passé commande chez nous !") 
