5f6fb6e51cbb03bc4ffeeee84810462cb793cc47
Day-101 <69306872+Day-101@users.noreply.github.com>
2021-03-11 17:57:27 +0100

    build: statics pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/contact'
+  get 'static_pages/privacy'
+  get 'static_pages/concept'
+  get 'static_pages/team'
 	root "items#index"
   resources :items
   devise_for :users
